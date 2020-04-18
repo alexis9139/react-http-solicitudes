2c2d8a5f334b129b430d91512657c54ce776b86b
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-18 19:52:01 -0300

    Ejemplo buscador de películas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    cargando: true
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => this.setState({ users, cargando: false }))
+  }
+
+  render() {
+    if (this.state.cargando) {
+      return <h1>Cargando...</h1>
+    }
+    return (
+      <div>
+        <h1>peticion HTTP</h1>
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>
+              {user.name}
+              <a href={`http://${user.website}`}>Web site</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
